7c63179ca7bd8fd20d5db58608b5003db52af2a1
janisfs <janisfs@mail.ru>
2024-07-05 23:10:54 +0300

    delete mistakes

--- Composition.py
+++ Composition.py
@@ -2,12 +2,14 @@ class Teacher():
     def teach(self):
         print("Преподаватель учит")
 
+
 class School():
     def __init__(self):
         self.teacher = Teacher()  # Создание учителя внутри школы
 
     def start_lesson(self):
         self.teacher.teach()
+        
 
 my_school = School()  # Учитель создается автоматически при создании школы
 my_school.start_lesson()  # Вызывает метод учителя через школу
